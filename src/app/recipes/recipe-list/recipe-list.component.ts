f06e7a1e28ae409f42c674b510224b2e306479e8
Shenyang Cai <sycai@outlook.com>
2018-12-24 21:34:37 -0500

    Enable recipe-detail component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -8,11 +8,17 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://api.norecipes.com/wp-content/'
+    new Recipe('A Test Recipe 1', 'This is simply a test 1', 'https://api.norecipes.com/wp-content/'
     + 'uploads/2018/08/teriyaki-chicken-recipe_007.jpg'),
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://api.norecipes.com/wp-content/'
+    new Recipe('A Test Recipe 2', 'This is simply a test 2', 'https://api.norecipes.com/wp-content/'
     + 'uploads/2018/08/teriyaki-chicken-recipe_007.jpg')
   ];
+
+  @Output() recipeSelected = new EventEmitter<Recipe>();
+
+  onSelected(recipe: Recipe) {
+    this.recipeSelected.emit(recipe);
+  }
 
   constructor() { }
 
