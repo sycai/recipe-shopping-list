3cd9f91fd4bc38c9490d9ea71386c905418fd158
Shenyang Cai <sycai@outlook.com>
2018-12-24 21:00:34 -0500

    Enable navigation for the header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'recipe-shopping-list';
+  loadedFeature = 'recipe';
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,13 @@
-import {Component } from '@angular/core';
+import {Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
-    selector: 'app-header',
-    templateUrl: 'header.component.html',
+  selector: 'app-header',
+  templateUrl: 'header.component.html',
 })
 export class HeaderComponent {
+  @Output() featureSelected = new EventEmitter<string>();
 
+  onSelect(item: string) {
+    this.featureSelected.emit(item);
+  }
 }
