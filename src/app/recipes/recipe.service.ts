f05c2d41efacab11036ee9bf5209869c94b60d65
Shenyang Cai <sycai@outlook.com>
2018-12-27 19:06:15 -0500

    Let recipe service handle recipe selection

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,9 @@
 import { Recipe } from './recipe.model';
+import { EventEmitter } from '@angular/core';
 
 export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
   private recipes: Recipe[] = [
     new Recipe('A Test Recipe 1', 'This is simply a test 1', 'https://api.norecipes.com/wp-content/'
     + 'uploads/2018/08/teriyaki-chicken-recipe_007.jpg'),
