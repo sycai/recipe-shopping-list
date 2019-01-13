73abbdbab64c2fda62c07cbaebcb12f9986953aa
Shenyang Cai <sycai@outlook.com>
2019-01-12 20:58:35 -0500

    Enable authentication

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,15 +1,24 @@
 import * as firebase from 'firebase';
 
 export class AuthService {
+  token: string;
+
   signupUser(email: string, password: string) {
     firebase.auth().createUserWithEmailAndPassword(email, password).catch(
-        error => console.log(error)
+      error => console.log(error)
     );
   }
 
   signinUser(email: string, password: string) {
     firebase.auth().signInWithEmailAndPassword(email, password)
-      .then(response => console.log(response))
+      .then(response => firebase.auth().currentUser.getIdToken().then((token: string) => this.token = token))
       .catch(error => console.log(error));
+  }
+
+  getToken() {
+    firebase.auth().currentUser.getIdToken().then(
+      (token: string) => this.token = token
+    );
+    return this.token;
   }
 }
